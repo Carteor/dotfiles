f547382bec5a49202473b9bd1645675aea96ef75
Serikov Sayat <simon@acsolutions.ai>
2025-07-03 17:31:25 +0500

    disable nvim-tree for neo-tree, enabled some kikstart plugins

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -27,49 +27,5 @@ return {
       vim.keymap.set('x', '<leader>y', require('osc52').copy_visual)
     end,
   },
-  {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-    keys = {
-      { '<leader>e', ':NvimTreeToggle<CR>', desc = 'Toggle nvim-tree' },
-    },
-    config = function()
-      require('nvim-tree').setup {
-        respect_buf_cwd = true,
-        update_focused_file = {
-          enable = true,
-          update_cwd = true,
-        },
-        on_attach = function(bufnr)
-          local api = require 'nvim-tree.api'
-          local function opts(desc)
-            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-          end
-          -- Navigation
-          vim.keymap.set('n', '<CR>', api.node.open.edit, opts 'Open')
-          vim.keymap.set('n', 'l', api.node.open.edit, opts 'Open')
-          vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts 'Close Directory')
-          vim.keymap.set('n', 'v', api.node.open.vertical, opts 'Open: Vertical Split')
-          vim.keymap.set('n', 's', api.node.open.horizontal, opts 'Open: Horizontal Split')
-          vim.keymap.set('n', 't', api.node.open.tab, opts 'Open: New Tab')
-          -- File operations
-          vim.keymap.set('n', 'a', api.fs.create, opts 'Create')
-          vim.keymap.set('n', 'd', api.fs.remove, opts 'Delete')
-          vim.keymap.set('n', 'r', api.fs.rename, opts 'Rename')
-          vim.keymap.set('n', 'x', api.fs.cut, opts 'Cut')
-          vim.keymap.set('n', 'c', api.fs.copy.node, opts 'Copy')
-          vim.keymap.set('n', 'p', api.fs.paste, opts 'Paste')
-          -- Tree operations
-          vim.keymap.set('n', 'R', api.tree.reload, opts 'Refresh')
-          vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts 'Toggle Hidden')
-          vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts 'Toggle Git Ignore')
-          -- Close tree
-          vim.keymap.set('n', 'q', api.tree.close, opts 'Close')
-        end,
-      }
-    end,
-  },
+  
 }
