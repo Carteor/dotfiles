6c8dfb1df2ea1038ce4ebc5d360793f23fe5021f
Serikov Sayat <simon@acsolutions.ai>
2025-07-23 16:30:02 +0500

    add mini modules to nvim

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -3,10 +3,12 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  'dhruvasagar/vim-zoom',
-  keys = {
-    { '<C-w>m', '<Plug>(zoom-toggle)', desc = 'Toggle Zoom' },
-  },
+    {
+        'dhruvasagar/vim-zoom',
+        keys = {
+            { '<C-w>m', '<Plug>(zoom-toggle)', desc = 'Toggle Zoom' },
+        },
+    },
   {
     'akinsho/toggleterm.nvim',
     version = '*',
@@ -27,25 +29,41 @@ return {
       vim.keymap.set('x', '<leader>y', require('osc52').copy_visual)
     end,
   },
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
-  },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  },
-  'echasnovski/mini.nvim',
-  version = false,
-  config = function()
-    require('mini.surround').setup()
-  end,
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+            "TmuxNavigatorProcessList",
+        },
+        keys = {
+            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
+    {
+        'echasnovski/mini.nvim',
+        version = false,
+        config = function()
+            require('mini.surround').setup()
+            require('mini.pairs').setup()
+            require('mini.comment').setup()
+            require('mini.ai').setup()
+            require('mini.indentscope').setup({
+                symbol = '|',
+                draw = {
+                    delay = 0,
+                    animation = require('mini.indentscope').gen_animation.none(),
+                },
+            })
+            -- Make indent scope subtle gray
+            vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { fg = '#555555', nocombine = true })
+        end,
+    },
 }
